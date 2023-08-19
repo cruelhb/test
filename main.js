23b215df9df0bfff35b6cdfbdb3eb5ea16a27b82
HB NA <hbna@HBui-MacBookAir.local>
2023-08-19 19:39:16 +0900

    프로젝트생성

---
+++ main.js
@@ -0,0 +1,14 @@
+// const link = document.querySelector('.container a');
+
+// link.addEventListener('click', function() {
+
+// })
+
+// addEventListener('이벤트',함수(이벤트핸들러))
+
+console.log("안녕하세요");
+
+const a = 3;
+const b = 5;
+
+console.log(a * b);
